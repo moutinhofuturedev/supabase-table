1c68bc0deb2992f2948300826af4243bcd344691
paulo <paulo_vicali@icloud.com>
2024-11-28 18:36:51 -0300

    feat(v2): add componente toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
+import { Toaster } from '@/components/ui/toaster'
 import { ReactQueryProvider } from '@/lib/react-query-provider'
 
 const geistSans = localFont({
@@ -30,6 +31,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
+				<Toaster />
 				<ReactQueryProvider>{children}</ReactQueryProvider>
 			</body>
 		</html>
