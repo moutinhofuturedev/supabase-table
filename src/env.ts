0abbb85f83ce5fb995370499786e32d5e4bf2999
paulo <paulo_vicali@icloud.com>
2024-11-30 01:19:58 -0300

    fix(main): resolve conflito de variável de ambiente

--- src/env.ts
+++ src/env.ts
@@ -4,18 +4,18 @@ import { z } from 'zod'
 export const env = createEnv({
 	server: {
 		SUPABASE_URL: z.string().url(),
-		SUPABASE_SECRET_KEY: z.string(),
+		SUPABASE_SECRET_KEY: z.string().min(1),
 	},
 
 	client: {
-		NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+		// NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
 		NEXT_PUBLIC_API_URL: z.string().url(),
 	},
 
 	runtimeEnv: {
 		SUPABASE_URL: process.env.SUPABASE_URL,
 		SUPABASE_SECRET_KEY: process.env.SUPABASE_SECRET_KEY,
-		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+		// NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
 		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
 	},
 })
